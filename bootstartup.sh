9717d0b27320ed071746eb1b521b9adfc6471edb
oliveglobal <ranjan1@oliveglobal.com>
2016-12-26 14:36:16 +0530

    Update bootstartup.sh

--- bootstartup.sh
+++ bootstartup.sh
@@ -1,14 +1,16 @@
+
 #!/bin/bash
 yum update -y
-yum install httpd php php-mysql mysql mysql-server -y
-chkconfig httpd on
-chkconfig mysqld on
+yum install httpd php php-mysql stress -y
+cd /etc/httpd/config/
+cp  httpd.config httpd.conf-bak
 service restart httpd
-service restart mysqld
-cp /etc/httpd/config/httpd.conf  /etc/httpd/config/httpd.conf-bak
+chkconfig httpd on
+cd /var/www/html/
 wget -q -O http://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 cp -r wordpress/* /var/www/html/
-chmod -R 755 /var/www/html/wp-content/
+chmod -R 755 wp-content
 chmod -R apache:apache /var/www/html/wp-content/
-rm -rf wordpress  latest.tar.gz
+rm -rf wordpress  
+rm -rf latest.tar.gz
